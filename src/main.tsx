1c6d604f4170dde4861a36756c63cae2aeea851b
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-11-12 11:29:08 -0600

    add package postcss, postcss-import, sass, tailwindcss

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router/router'
+import './index.css'
+import './style/style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
