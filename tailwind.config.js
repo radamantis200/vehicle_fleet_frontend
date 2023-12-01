09d3f1e0ee0c911bd9671577bfcc23aaa65c9d60
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-11-30 22:45:56 -0600

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}", "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {
-      screens: {
-        'sm': '1100px',
-        'lg': '1100px',
-        'xl': '1400px',
-      },
-    }
+    extend: {    }
   },
   plugins: [require('flowbite/plugin')],
 }
