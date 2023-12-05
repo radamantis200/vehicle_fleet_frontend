a4d64b6873a39731e9dca6d8dd5bf477fa032814
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2023-12-04 22:27:02 -0600

    add packages react-query react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client'
 import '@/index.css'
 import router from './router/router'
 import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+// Create a client
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
