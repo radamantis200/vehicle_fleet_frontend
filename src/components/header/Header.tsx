0673340e71be8a389069bd57f2b484eae658aade
Jefry Sanchez <jfsanchez@lafise.com>
2023-12-05 08:44:21 -0600

    remove meta twitter, facebook remove url link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -25,11 +25,11 @@ const Header = () => {
                     clipRule="evenodd"></path>
                 </svg>
               </button>
-              <a href="https://flowbite-admin-dashboard.vercel.app/" className="flex ml-2 md:mr-24">
+              <a href="#" className="flex ml-2 md:mr-24">
                 <img src="https://flowbite-admin-dashboard.vercel.app/images/logo.svg" className="h-8 mr-3"
                   alt="FlowBite Logo" />
                 <span
-                  className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+                  className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Vehicle</span>
               </a>
               <form action="#" method="GET" className="hidden lg:block lg:pl-3.5">
                 <label htmlFor="topbar-search" className="sr-only">Search</label>
