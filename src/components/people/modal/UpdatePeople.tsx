264e2946eb3d0e29a974982bdf18ec47453656f9
Jefry Sanchez <jfsanchez@lafise.com>
2023-12-23 09:34:55 -0600

    update

---
+++ src/components/people/modal/UpdatePeople.tsx
@@ -0,0 +1,74 @@
+import { initFlowbite } from "flowbite"
+import { useEffect } from "react"
+
+const UpdatePeople = () => {
+    useEffect(() => {
+        initFlowbite()
+    }, [])
+    return <>
+        <button data-modal-target="crud-modal" data-modal-toggle="crud-modal" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800" type="button">
+            <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z">
+                </path>
+                <path fill-rule="evenodd"
+                    d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                    clip-rule="evenodd"></path>
+            </svg>
+            Editar Persona
+        </button>
+
+        <div id="crud-modal" tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+            <div className="relative p-4 w-full max-w-md max-h-full">
+                <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+
+                    <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                        <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                            Edit People
+                        </h3>
+                        <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
+                            <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                            </svg>
+                            <span className="sr-only">Close modal</span>
+                        </button>
+                    </div>
+                    <form className="p-4 md:p-5">
+                        <div className="grid gap-4 mb-4 grid-cols-2">
+                            <div className="col-span-2">
+                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required={true} />
+                            </div>
+                            <div className="col-span-2 sm:col-span-1">
+                                <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
+                                <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required={true} />
+                            </div>
+                            <div className="col-span-2 sm:col-span-1">
+                                <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                                <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                    <option selected={true}>Select category</option>
+                                    <option value="TV">TV/Monitors</option>
+                                    <option value="PC">PC</option>
+                                    <option value="GA">Gaming/Console</option>
+                                    <option value="PH">Phones</option>
+                                </select>
+                            </div>
+                            <div className="col-span-2">
+                                <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
+                                <textarea id="description" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>
+                            </div>
+                        </div>
+                        <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg className="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path></svg>
+                            Save all
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </>
+}
+
+export default UpdatePeople
+
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,3 +1,5 @@
+import DeletePeople from "@/components/people/modal/DeletePeople"
+import UpdatePeople from "@/components/people/modal/UpdatePeople"
 import formatDate from "@/helpers/data"
 import usePeople from "@/hooks/usePeople"
 
@@ -147,29 +149,8 @@ const Home = () => {
                                                     </div>
                                                 </td>
                                                 <td className="p-4 space-x-2 whitespace-nowrap">
-                                                    <button type="button" data-modal-toggle="edit-user-modal"
-                                                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800">
-                                                        <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
-                                                            xmlns="http://www.w3.org/2000/svg">
-                                                            <path
-                                                                d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z">
-                                                            </path>
-                                                            <path fill-rule="evenodd"
-                                                                d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
-                                                                clip-rule="evenodd"></path>
-                                                        </svg>
-                                                        Editar Persona
-                                                    </button>
-                                                    <button type="button" data-modal-toggle="delete-user-modal"
-                                                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900">
-                                                        <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20"
-                                                            xmlns="http://www.w3.org/2000/svg">
-                                                            <path fill-rule="evenodd"
-                                                                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                                                clip-rule="evenodd"></path>
-                                                        </svg>
-                                                        Eliminar Persona
-                                                    </button>
+                                                    <UpdatePeople />
+                                                    <DeletePeople />
                                                 </td>
                                             </tr>
                                         ))}
@@ -227,99 +208,6 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="sticky z-50 lg:z-[51] bottom-0 right-0 items-center w-full p-4  border-t border-gray-200 sm:flex sm:justify-between dark:bg-gray-800 dark:border-gray-700">
-                    <div className="fixed left-0 right-0  items-center justify-center hidden overflow-x-hidden overflow-y-auto top-4 md:inset-0 h-modal sm:h-full"
-                        id="edit-user-modal">
-                        <div className="relative w-full h-full max-w-2xl px-4 md:h-auto">
-
-                            <div className="relative bg-white rounded-lg shadow dark:bg-gray-800">
-
-                                <div className="flex items-start justify-between p-5 border-b rounded-t dark:border-gray-700">
-                                    <h3 className="text-xl font-semibold dark:text-white">
-                                        Edit user
-                                    </h3>
-                                    <button type="button"
-                                        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-700 dark:hover:text-white"
-                                        data-modal-toggle="edit-user-modal">
-                                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
-                                    </button>
-                                </div>
-
-                                <div className="p-6 space-y-6">
-                                    <form action="#">
-                                        <div className="grid grid-cols-6 gap-6">
-                                            <div className="col-span-6 sm:col-span-3">
-                                                <label htmlFor="first-name"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First
-                                                    Name</label>
-                                                <input type="text" name="first-name" value="Bonnie" id="first-name"
-                                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                    placeholder="Bonnie" required={true} />
-                                            </div>
-                                            <div className="col-span-6 sm:col-span-3">
-                                                <label htmlFor="last-name"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last
-                                                    Name</label>
-                                                <input type="text" name="last-name" value="Green" id="last-name"
-                                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                    placeholder="Green" required={true} />
-                                            </div>
-                                            <div className="col-span-6 sm:col-span-3">
-                                                <label htmlFor="email"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                                <input type="email" name="email" value="bonnie@flowbite.com" id="email"
-                                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                    placeholder="example@company.com" required={true} />
-                                            </div>
-                                            <div className="col-span-6 sm:col-span-3">
-                                                <label htmlFor="position"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Position</label>
-                                                <input type="text" name="position" value="React Developer" id="position"
-                                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                    placeholder="e.g. React developer" required={true} />
-                                            </div>
-                                            <div className="col-span-6 sm:col-span-3">
-                                                <label htmlFor="current-password"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Current
-                                                    Password</label>
-                                                <input type="password" name="current-password" value="••••••••"
-                                                    id="current-password"
-                                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                    placeholder="••••••••" required={true} />
-                                            </div>
-                                            <div className="col-span-6 sm:col-span-3">
-                                                <label htmlFor="new-password"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New
-                                                    Password</label>
-                                                <input type="password" name="new-password" value="••••••••"
-                                                    id="new-password"
-                                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                    placeholder="••••••••" required={true} />
-                                            </div>
-                                            <div className="col-span-6">
-                                                <label htmlFor="biography"
-                                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Biography</label>
-                                                <textarea id="biography" rows={4}
-                                                    className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                                    placeholder="👨‍💻Full-stack web developer. Open-source contributor.">👨‍💻Full-stack web developer. Open-source contributor.</textarea>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-
-                                <div className="items-center p-6 border-t border-gray-200 rounded-b dark:border-gray-700">
-                                    <button
-                                        className="text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-blue-700 dark:focus:ring-primary-800"
-                                        type="submit">Save all</button>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
                     <div className="fixed left-0 right-0  items-center justify-center hidden overflow-x-hidden overflow-y-auto top-4 md:inset-0 h-modal sm:h-full"
                         id="add-user-modal">
                         <div className="relative w-full h-full max-w-2xl px-4 md:h-auto">
@@ -392,46 +280,6 @@ const Home = () => {
                                         type="submit">Add user</button>
                                 </div>
 
-                            </div>
-                        </div>
-                    </div>
-                    <div className="fixed left-0 right-0  items-center justify-center hidden overflow-x-hidden overflow-y-auto top-4 md:inset-0 h-modal sm:h-full"
-                        id="delete-user-modal">
-                        <div className="relative w-full h-full max-w-md px-4 md:h-auto">
-
-                            <div className="relative bg-white rounded-lg shadow dark:bg-gray-800">
-
-                                <div className="flex justify-end p-2">
-                                    <button type="button"
-                                        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-700 dark:hover:text-white"
-                                        data-modal-toggle="delete-user-modal">
-                                        <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
-                                    </button>
-                                </div>
-
-                                <div className="p-6 pt-0 text-center">
-                                    <svg className="w-16 h-16 mx-auto text-red-600" fill="none" stroke="currentColor"
-                                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                            d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                                    </svg>
-                                    <h3 className="mt-5 mb-6 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to
-                                        delete this user?</h3>
-                                    <a href="#"
-                                        className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2 dark:focus:ring-red-800">
-                                        Yes, I'm sure
-                                    </a>
-                                    <a href="#"
-                                        className="text-gray-900 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-primary-300 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                                        data-modal-toggle="delete-user-modal">
-                                        No, cancel
-                                    </a>
-                                </div>
                             </div>
                         </div>
                     </div>
