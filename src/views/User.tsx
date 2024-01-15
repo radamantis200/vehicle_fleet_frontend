5a0c1071182f6fae6573863a29139a17df0ca06b
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2024-01-14 19:38:07 -0600

    update

--- src/views/User.tsx
+++ src/views/User.tsx
@@ -18,7 +18,7 @@ const User = () => {
                     className="p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700">
                     <div className="w-full mb-1">
                         <div className="mb-4">
-                            <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Usuarioes</h1>
+                            <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">Usuarios</h1>
                         </div>
                         <div className="md:flex">
                             <div
