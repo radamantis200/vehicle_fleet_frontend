eb433d6f5b0f0b9231ee0e7fd84fc2fd227c0cf0
Jefry Sanchez <sanchezmorajeffrey@gmail.com>
2024-06-22 21:23:58 -0600

    se esta utilizando los componente de formularios

--- src/components/people/modal/AddPeople.tsx
+++ src/components/people/modal/AddPeople.tsx
@@ -1,6 +1,44 @@
-import { Datepicker } from 'flowbite-react'
+import { useFormik } from 'formik';
+import PeopleValidationSchema from '@/validation/PeopleValidation';
+import useGender from "@/hooks/useGender"
+import { Gender } from '@/types/genderType';
+import FormInput from '@/components/form/FormInput';
+import SelectForm from '@/components/form/FormSelect'
+import FormTextArea from '@/components/form/FormTextArea';
+import moment from 'moment';
+import FormDatePicker from '@/components/form/FormDatePicker';
+
+const transformGenderData = (data: Gender[]): { value: number, label: string }[] => {
+    return data.map(item => ({
+        value: item.codGenero,
+        label: item.genero,
+    }));
+};
 
 const AddPeople = () => {
+    const date = moment().format();
+    const { genderQuery } = useGender()
+    const { data: gender } = genderQuery;
+
+    const genderOptions = gender ? transformGenderData(gender.response) : [];
+
+    const formik = useFormik({
+        initialValues: {
+            nombres: '',
+            apellidos: '',
+            correo: '',
+            nacionalidad: '',
+            catGeneroPersona_codGeneroPerosna: '',
+            identificacion: '',
+            direccion: '',
+            fechaNacimiento: date,
+        },
+        validationSchema: PeopleValidationSchema,
+        onSubmit: (values) => {
+            console.log(values);
+        },
+    });
+
     return <>
         <div id="add-people" tabIndex={-1} aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
             <div className="relative p-4 w-full max-w-md max-h-full">
@@ -17,40 +55,110 @@ const AddPeople = () => {
                             <span className="sr-only">Close modal</span>
                         </button>
                     </div>
-                    <form className="p-4 md:p-5">
+                    <form className="p-4 md:p-5" onSubmit={formik.handleSubmit}>
                         <div className="grid gap-4 mb-4 grid-cols-2">
                             <div className="col-span-2">
-                                <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombres</label>
-                                <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nombres" required={true} />
+                                <FormInput
+                                    label="Nombres"
+                                    name="nombres"
+                                    id="nombres"
+                                    type="text"
+                                    placeholder="Nombres"
+                                    value={formik.values.nombres}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.nombres && formik.errors.nombres ? formik.errors.nombres : undefined}
+                                />
                             </div>
                             <div className="col-span-2">
-                                <label htmlFor="lastname" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Apellidos</label>
-                                <input type="text" name="lastname" id="lastname" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Apellidos" required={true} />
+                                <FormInput
+                                    label="Apellidos"
+                                    name="apellidos"
+                                    id="apellidos"
+                                    type="text"
+                                    placeholder="Apellidos"
+                                    value={formik.values.apellidos}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.apellidos && formik.errors.apellidos ? formik.errors.apellidos : undefined}
+                                />
                             </div>
                             <div className="col-span-2">
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Correo electrónico</label>
-                                <input type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Correo electrónico" required={true} />
+                                <FormInput
+                                    label="Identificacion"
+                                    name="identificacion"
+                                    id="identificacion"
+                                    type="text"
+                                    placeholder="Nacionalidad"
+                                    value={formik.values.identificacion}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.identificacion && formik.errors.identificacion ? formik.errors.identificacion : undefined}
+                                />
                             </div>
                             <div className="col-span-2">
-                                <label htmlFor="nationality" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nacionalidad</label>
-                                <input type="text" name="nationality" id="nationality" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nacionalidad" required={true} />
+                                <FormInput
+                                    label="Correo electrónico"
+                                    name="correo"
+                                    id="correo"
+                                    type="email"
+                                    placeholder="Correo electrónico"
+                                    value={formik.values.correo}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.correo && formik.errors.correo ? formik.errors.correo : undefined}
+                                />
                             </div>
                             <div className="col-span-2">
-                                <label htmlFor="Genero" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Genero</label>
-                                <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                    <option defaultValue="0">Seleccione</option>
-                                    <option value="TV">TV/Monitors</option>
-                                    <option value="PC">PC</option>
-                                    <option value="GA">Gaming/Console</option>
-                                    <option value="PH">Phones</option>
-                                </select>
-                            </div>
-                            <div className="relative max-w-sm">
-                                <Datepicker />
+                                <FormInput
+                                    label="Nacionalidad"
+                                    name="nacionalidad"
+                                    id="nacionalidad"
+                                    type="text"
+                                    placeholder="Nacionalidad"
+                                    value={formik.values.nacionalidad}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.nacionalidad && formik.errors.nacionalidad ? formik.errors.nacionalidad : undefined}
+                                />
                             </div>
                             <div className="col-span-2">
-                                <label htmlFor="direccion" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Dirección</label>
-                                <textarea id="direccion" rows={4} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Dirección"></textarea>
+                                <label htmlFor="gender" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Género</label>
+                                <SelectForm
+                                    data={genderOptions}
+                                    defaultOption="Seleccione"
+                                    id="catGeneroPersona_codGeneroPerosna"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    value={formik.values.catGeneroPersona_codGeneroPerosna}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.catGeneroPersona_codGeneroPerosna && formik.errors.catGeneroPersona_codGeneroPerosna ? formik.errors.catGeneroPersona_codGeneroPerosna : undefined}
+                                />
+                            </div>
+                            <div className="relative">
+                                <FormDatePicker
+                                    value={formik.values.fechaNacimiento}
+                                    onDateChange={(date) => formik.setFieldValue('fechaNacimiento', date)}
+                                    label="Fecha de Nacimiento"
+                                    inline={false}
+                                    maxDate={new Date()}
+                                    language='es-NI'
+                                    showClearButton={false}
+                                    showTodayButton={false}
+                                    title={"Fecha de Nacimiento"}
+                                />
+                            </div>
+                            <div className="col-span-2">
+                                <FormTextArea
+                                    label="Descripción"
+                                    name="direccion"
+                                    id="direccion"
+                                    placeholder="Escribe una descripción..."
+                                    value={formik.values.direccion}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    error={formik.touched.direccion && formik.errors.direccion ? formik.errors.direccion : undefined}
+                                />
                             </div>
                         </div>
                         <button type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
